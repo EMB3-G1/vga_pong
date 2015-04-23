e918478bc3ac2eaa961c61433b5c459b2b821619
Ignaciotb <ignaciotb>
2015-04-23 17:38:31 +0200

    median filter cleaned, still under tests

--- EMB3-PROJECT/emb3_board_test/sliding_average.vhd
+++ EMB3-PROJECT/emb3_board_test/sliding_average.vhd
@@ -36,7 +36,7 @@ end sliding_average;
 architecture Behavioral of sliding_average is
 
 	----------------------------------------------------------------
-	-- constant declarations
+	-- constant declarations: length of the mask
 	----------------------------------------------------------------
 		constant C_DATA_CNT 	: integer := 21;
 	----------------------------------------------------------------
@@ -51,63 +51,21 @@ architecture Behavioral of sliding_average is
 	-- signal declarations
 	----------------------------------------------------------------
 		signal slav_delay					: slav_array_type(0 to C_DATA_CNT-1) := (others=>(others=>'0'));
-		signal sum_sig 					: unsigned(13 downto 0 );
-		signal division_dividend		: unsigned(13 downto 0);
-		signal division_quotient		: unsigned(13 downto 0);
-		signal division_quotient_slv	: std_logic_vector(13 downto 0);
-	----------------------------------------------------------------
-	
-	----------------------------------------------------------------
-	-- component declarations
-	----------------------------------------------------------------
-		component divider
-			port (
-			clk: in std_logic;
-			rfd: out std_logic;
-			dividend: in std_logic_vector(13 downto 0);
-			divisor: in std_logic_vector(3 downto 0);
-			quotient: out std_logic_vector(13 downto 0);
-			fractional: out std_logic_vector(3 downto 0));
-		end component;
-	----------------------------------------------------------------
-	
-begin
-
-	----------------------------------------------------------------
-	-- Clock outputs for ADC and DAC
-	----------------------------------------------------------------
-		adc_clk_o <= filter_clk;
-		--dac_clk_o <= filter_clk;
-	----------------------------------------------------------------
-	
-	----------------------------------------------------------------
-	-- Divider core
-	----------------------------------------------------------------	
-		divider_inst : divider
-		port map (
-			clk 			=> filter_clk,
-			rfd 			=> open,
-			dividend 	=> std_logic_vector(division_dividend),
-			divisor 		=> std_logic_vector(to_unsigned(C_DATA_CNT,4)),
-			quotient 	=> division_quotient_slv,
-			fractional 	=> open
-			);		
 		
-		division_quotient <= unsigned(division_quotient_slv);
-	----------------------------------------------------------------
+begin
 	
 	----------------------------------------------------------------
-	-- Sliding Average logic 
+	-- Median average 
 	----------------------------------------------------------------
 		process(filter_clk)
-			--variable sum	: unsigned(13 downto 0) := (others => '0');
 			variable counter_black : integer range 0 to C_DATA_CNT+1:=C_DATA_CNT;
 			variable counter_white : integer range 0 to C_DATA_CNT+1:=0;
 
 		begin
 			if rising_edge(filter_clk) then
 				if(filter_reset = '1') then
-					sum_sig <= (others => '0');
+					counter_black :=C_DATA_CNT;
+					counter_white :=0;
 				else
 					
 					slav_delay(0) <= unsigned(adc_i);
@@ -137,8 +95,5 @@ begin
 				end if;
 			end if;
 		end process;
-		
-		division_dividend <= unsigned(sum_sig);
-	----------------------------------------------------------------
 
 end Behavioral;
